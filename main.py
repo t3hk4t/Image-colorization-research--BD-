630489e96477f2dcb206bcd2e854b14d74a1918b
Leonards <leonards.elksnis@gmail.com>
2021-03-07 18:33:49 +0200

    lol

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import matplotlib
 from modules_core import dummy_loader
 import argparse
 from skimage import color
+from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
+import torchvision
 import time
-import matplotlib.pyplot as plt
+import sys
 from modules import tensorboard_utils
 import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
@@ -16,10 +18,10 @@ import torch.utils.data
 
 parser = argparse.ArgumentParser(description='Model trainer')
 parser.add_argument('-run_name', default=f'run_{time.time()}', type=str)
-parser.add_argument('-sequence_name', default=f'../../seq_default', type=str)
+parser.add_argument('-sequence_name', default=f'../seq_default', type=str)
 parser.add_argument('-is_cuda', default=True, type=lambda x: (str(x).lower() == 'true'))
 parser.add_argument('-learning_rate', default=1e-4, type=float)
-parser.add_argument('-batch_size', default=1, type=int)
+parser.add_argument('-batch_size', default=10, type=int)
 parser.add_argument('-epochs', default=1000, type=int)
 parser.add_argument('-is_deep_supervision', default=True, type=bool)
 parser.add_argument('-unet_depth', default=5, type=int)
@@ -28,9 +30,9 @@ parser.add_argument('-expansion_rate', default=3, type=int)
 # TODO add more params and make more beautitfull cuz this file is a mess
 args = parser.parse_args()
 
-summary_writer = tensorboard_utils.CustomSummaryWriter(
-    log_dir=f'{args.sequence_name}/{args.run_name}'
-)
+
+writer = SummaryWriter(f'{args.sequence_name}/{args.run_name}')
+tensorboard_writer = tensorboard_utils.TensorBoardUtils(writer)
 
 
 USE_CUDA = torch.cuda.is_available()
@@ -38,77 +40,116 @@ MAX_LEN = 200 # limit max number of samples otherwise too slow training (on GPU 
 if USE_CUDA:
     MAX_LEN = None
 
-dataset = dummy_loader.SyntheticNoiseDataset(augmented_directory=r'C:\Users\37120\Documents\BachelorThesis\image_data\flick30k_10_augmented0',
-                                             greyscale_directory=r'C:\Users\37120\Documents\BachelorThesis\image_data\flick30k_10_test')
-train_size = int(0.8 * len(dataset))
-print(len(dataset))
-test_size = len(dataset) - train_size
-dataset_train, dataset_test = torch.utils.data.random_split(dataset, [train_size, test_size])
+dataset_train = dummy_loader.SyntheticNoiseDataset(train=True)
+dataset_test = dummy_loader.SyntheticNoiseDataset(train=False)
 
-data_loader_train = torch.utils.data.DataLoader(
-    dataset=dataset_train,
-    batch_size=args.batch_size,
-)
-data_loader_test = torch.utils.data.DataLoader(
-    dataset=dataset_test,
-    batch_size=args.batch_size,
-)
+i = 0
+j = 0
+for sample in tqdm(dataset_train):
+    y = sample['greyscale_image']
+    x = sample['augmented_image']
+    x = torch.squeeze(x, axis=0)
+    y = torch.squeeze(y, axis=0)
+    for sample in tqdm(dataset_train):
+        if i == 0:
+            i+=1
+            continue
+        y_2 = sample['greyscale_image']
+        x_2 = sample['augmented_image']
+        x_2 = torch.squeeze(x_2, axis=0)
+        y_2 = torch.squeeze(y_2, axis=0)
+        x = torch.cat([x, x_2], 0)
+        y = torch.cat([y, y_2], 0)
+        j+=1
+        if j > 5:
+            img_grid_train_greyscale = torchvision.utils.make_grid(y)
+            img_grid_train_augmented = torchvision.utils.make_grid(x)
+            writer.add_image("train_greyscale", img_grid_train_greyscale)
+            writer.add_image("train_augmented", img_grid_train_augmented)
+            break
 
-#for data_loader in [data_loader_train, data_loader_test]:
-#    stage = 'train'
-#    if data_loader == data_loader_test:
-#        stage = 'test'
-#    for sample in tqdm(data_loader):
-#        y = sample['greyscale_image']
-#        x = sample['augmented_image']
-#
-#        x_img = np.zeros(shape=(320,480,3))
-#        y_img = np.zeros(shape=(320, 480, 3))
-#
-#        y_img[:,:,0] = y[0,0,:,:]
-#
-#        x_img[:,:,0] = x[0, 0, :, :]
-#        print(y_img.shape)
-#        print(y_img)
-#        y_img = y_img * 100
-#        x_img = x_img * 100
-#        image1 = color.lab2rgb(y_img)
-#        image2 = color.lab2rgb(x_img)
-#
-#        plt.title(f'{stage}')
-#        plt.imshow(image2)
-#        plt.show()
+    i+=1
+    if i>=1:
+        break
+i = 0
+j = 0
+for sample in tqdm(dataset_test):
+    y = sample['greyscale_image']
+    x = sample['augmented_image']
+    x = torch.squeeze(x, axis=0)
+    y = torch.squeeze(y, axis=0)
+    for sample in tqdm(dataset_test):
+        if i == 0:
+            i += 1
+            continue
+        y_2 = sample['greyscale_image']
+        x_2 = sample['augmented_image']
+        x_2 = torch.squeeze(x_2, axis=0)
+        y_2 = torch.squeeze(y_2, axis=0)
+        x = torch.cat([x, x_2], 0)
+        y = torch.cat([y, y_2], 0)
+        j += 1
+        if j > 5:
+            img_grid_test_greyscale = torchvision.utils.make_grid(y)
+            img_grid_test_augmented = torchvision.utils.make_grid(x)
+            writer.add_image("test_greyscale", img_grid_test_greyscale)
+            writer.add_image("test_augmented", img_grid_test_augmented)
+            break
+
+    i += 1
+    if i >= 1:
+        break
+
 
 model = unetplusplus.Model(args)
-
 loss_func = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+
+for sample in tqdm(dataset_train):
+    x = sample['augmented_image']
+    x = x.float()
+    writer.add_graph(model, x)
+    break
+
 
 if USE_CUDA:
     model = model.cuda()
     loss_func = loss_func.cuda()
 
 metrics = {}
+
+
+image_metrics = {
+    'Greyscale_image': [],
+    'Augmented_image': []
+}
+
+
 for stage in ['train', 'test']:
     for metric in [
         'loss',
     ]:
         metrics[f'{stage}_{metric}'] = []
 
+epoch_test_loss = 0.0
+epoch_train_loss = 0.0
+running_test_loss = 0.0
+running_train_loss = 0
 
+i= 0
 for epoch in range(1, args.epochs):
 
-    for data_loader in [data_loader_train, data_loader_test]:
+    for data_loader in [dataset_train, dataset_test]:
         metrics_epoch = {key: [] for key in metrics.keys()}
-
+        n_total_steps = 0
         stage = 'train'
-        if data_loader == data_loader_test:
+        if data_loader == dataset_test:
             stage = 'test'
 
         for sample in tqdm(data_loader):
             y = sample['greyscale_image']
             x = sample['augmented_image']
-
+            i+=1
             y = y.float()
             x = x.float()
             if USE_CUDA:
@@ -119,30 +160,12 @@ for epoch in range(1, args.epochs):
             loss = loss_func.forward(y_prim, y)
             metrics_epoch[f'{stage}_loss'].append(loss.item()) # Tensor(0.1) => 0.1f
 
-            summary_writer.add_hparams(
-                hparam_dict=args.__dict__,
-                    metric_dict={
-                    'test_loss':loss.item(),
-                }
-            )
+            if data_loader == dataset_train:
+                running_train_loss += loss.item()
+            else:
+                running_test_loss += loss.item()
 
-            summary_writer.add_scalar(
-                tag=f'{data_loader}_loss',
-                scalar_value=loss.item(),
-                global_step=epoch
-            )
-
-            if data_loader == data_loader_train:
-                summary_writer.add_hparams(
-                    hparam_dict=args.__dict__,
-                    metric_dict={
-                        'train_loss': loss.item()
-                    }
-                )
-
-            summary_writer.flush()
-
-            if data_loader == data_loader_train:
+            if data_loader == dataset_train:
                 loss.backward()
                 optimizer.step()
                 optimizer.zero_grad()
@@ -152,32 +175,43 @@ for epoch in range(1, args.epochs):
             x = x.cpu()
             y = y.cpu()
 
-            np_y_prim = y_prim.data.numpy()
+
+            if i%100 == 0:
+                np_y_prim = torch.squeeze(y_prim, axis=0)
+                np_x = torch.squeeze(x, axis=0)
+                x_2 = torch.squeeze(y, axis=0)
+
+                data = torch.cat([x_2, np_x, np_y_prim], 1)
+
+                img_grid_validate = torchvision.utils.make_grid(data)
+                writer.add_image(f'Validate{i}', data, dataformats='HW')
+
+                if data_loader == dataset_train:
+                    writer.add_scalar('Train loss', running_train_loss/100, epoch * len(dataset_train) + n_total_steps)
+                else:
+                    writer.add_scalar('Train loss', running_test_loss / 100,
+                                      epoch * len(dataset_train) + n_total_steps)
+                running_train_loss = 0.0
+                running_test_loss = 0.0
+            n_total_steps+=1
 
 
         metrics_strs = []
         for key in metrics_epoch.keys():
             if stage in key:
                 value = np.mean(metrics_epoch[key])
+                if stage == 'train':
+                    epoch_train_loss = value
+                else:
+                    epoch_test_loss = value
                 metrics[key].append(value)
                 metrics_strs.append(f'{key}: {round(value, 2)}')
-
-        plt.clf()
-        plts=[]
-        c = 0
-        for key, value in metrics.items():
-            plts += plt.plot(value, f'C{c}', label=key)
-            c += 1
-        plt.legend(plts, [it.get_label() for it in plts])
-        plt.draw()
-        plt.pause(0.1)
-
-        summary_writer.flush()
 
         print(f'epoch: {epoch} {" ".join(metrics_strs)}')
 
 
-summary_writer.close()
+
+writer.close()
 input('quit?')
 
 # TODO - Logging at the end of epoch
